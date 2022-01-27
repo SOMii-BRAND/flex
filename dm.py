8e7105de200f7610a07aa53967f591f7e325a0c1
S O م i <68531283+SO-MI-AWAN@users.noreply.github.com>
2022-01-27 18:21:47 +0500

    Add files via upload

---
+++ dm.py
@@ -0,0 +1,14 @@
+import os, sys
+if __name__ == "__main__":
+	if len(sys.argv) == 2:
+		if sys.argv[1] == "remove":
+			os.system("rm -f .__apikey__.txt")
+			exit(" [!] Succesfull Deleted")
+		else:
+			print(" [?] Wellcome : ")
+			exit(" [!] Run : python jssl.py remove")
+	try:
+		__import__("dm").__main__Main()
+	except Exception as e:
+		exit(str(e))
+ 
