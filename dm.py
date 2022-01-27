95b2bb855856cb3f69b6bda049cf233f835a32f7
S O م i <68531283+SO-MI-AWAN@users.noreply.github.com>
2022-01-27 18:22:19 +0500

    Update dm.py

--- dm.py
+++ dm.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 			print(" [?] Wellcome : ")
 			exit(" [!] Run : python jssl.py remove")
 	try:
-		__import__("dm").__main__Main()
+		__import__("mx").__main__Main()
 	except Exception as e:
 		exit(str(e))
  
