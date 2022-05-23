49b3802582845a0d0adc3251419f6a84a16b9351
S O م i <68531283+SOMii-BRAND@users.noreply.github.com>
2022-05-23 12:43:19 +0500

    Update splus.py

--- splus.py
+++ splus.py
@@ -7,10 +7,10 @@ from time import sleep
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from fp import xxxxworldxxxx
-    print("\n Congratulations! Your device supported!\n")
-    time.sleep(3)
-    xxxxworldxxxx()
+    from fp import _site_view_
+    print("\n\n")
+    time.sleep(1)
+    _site_view_()
 elif bit == '32bit':
     from sp import _site_view_
     print("\n Congratulations! Your device supported!\n")
