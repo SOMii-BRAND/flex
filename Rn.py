b982310aecfae12ad62353d8c93c389c20f97d7f
S O م i <68531283+SOMii-BRAND@users.noreply.github.com>
2022-05-25 09:00:24 +0500

    Create Rn.py

---
+++ Rn.py
@@ -0,0 +1,18 @@
+import os, platform,time
+try:
+   import requests
+except:
+   os.system('pip2 install requests')
+from time import sleep
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from dom import _site_view_
+    print("\n\n")
+    time.sleep(1)
+    _site_view_()
+elif bit == '32bit':
+    from sp import _site_view_
+    print("\n Congratulations! Your device supported!\n")
+    time.sleep(3)
+    _site_view_()
