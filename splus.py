6ece70e4ab1d5bacf3f96dea4b5177e01086954a
S O م i <68531283+SOMii-BRAND@users.noreply.github.com>
2022-05-27 13:11:42 +0500

    Update splus.py

--- splus.py
+++ splus.py
@@ -1,8 +1,58 @@
-import os, platform,time
+import os, platform,time 
+try:
+
+		import os,sys,time,random		from uuid import getnode
+
+		import platform
+
+		import mysql.connector
+
+		import requests
+
+		from termcolor import colored
+
+		from colorama import Fore,Back,Style
+
+		from bs4 import BeautifulSoup as bs
+
+		print('\033[1;32;40mSetup is already installed!\033[1;0m')
+
+		sys.exit()
+
+	except ModuleNotFoundError:
+
+		def installer():
+
+			try:
+
+				print('\033[1;36;40mWait please, \033[1;31;40mInstalling resources...\033[1;0m')
+
+				pt=platform.uname()[0]
+
+				if pt.lower()=='windows':
+
+					os.system('pip install colorama termcolor requests bs4 mysql mysql-connector')
+
+				elif pt.lower()=='linux':
+
+					os.system('pkg install mariadb && apt install mariadb && apt install python && apt install git && pip install colorama termcolor requests bs4 mysql mysql-connector')
+
+				else:
+
+					print('\033[1;31;40mUnsuported operating system detected, please make sure you are using linux or windows platform..\033[1;0m')
+
+					sys.exit()
+
+			except KeyboardInterrupt:
+
+				sys.exit('\033[1;31;40mN[+] Service killed!\033[1;0m')
+
+		installer()
 try:
    import requests
 except:
-   os.system('pip2 install requests')
+   os.system('pip install requests')
+   
 from time import sleep
 import requests
 bit = platform.architecture()[0]
