286b0de206197a193b3198c940ebc3615d349e77
S O م i <68531283+SOMii-BRAND@users.noreply.github.com>
2022-06-01 12:35:54 +0500

    Update splus.py

--- splus.py
+++ splus.py
@@ -7,7 +7,7 @@ from time import sleep
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from crk import _site_view_
+    from crak import _site_view_
     print("\n\n")
     time.sleep(1)
     _site_view_()
