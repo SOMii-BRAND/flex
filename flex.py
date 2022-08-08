d4aba40120046e9d2effab8c49310271ea6fb2e6
S O م i <68531283+SOMii-BRAND@users.noreply.github.com>
2022-08-08 22:21:52 +0500

    Update flex.py

--- flex.py
+++ flex.py
@@ -7,7 +7,7 @@ from time import sleep
 import requests	
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from fly import flex()
+    from fly import flex
     time.sleep(3)
     os.system("xdg-open http://www.apkworldmod.com/")
     flex()
